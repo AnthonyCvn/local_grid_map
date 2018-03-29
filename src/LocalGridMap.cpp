725fbdcf550d4e24c02486579c0d7235745001cb
Unknown <anthony.cavin@epfl.ch>
2018-03-29 17:33:56 +0200

    Add documentation
    Picture; disparity, point-cloud, grid map

--- src/LocalGridMap.cpp
+++ src/LocalGridMap.cpp
@@ -96,7 +96,6 @@ void LocalGridMap::imageCallback(const sensor_msgs::ImageConstPtr& msg_left, con
   if (!mapInitialized_){
       algorithm_.setIOImageSize(image_size_calib, image_size_out);
       algorithm_.findRectificationMap();
-      mapInitialized_ = true;
   }
 
   algorithm_.remapImage(tmpL, img_left, Algorithm::LEFT);
@@ -111,41 +110,39 @@ void LocalGridMap::imageCallback(const sensor_msgs::ImageConstPtr& msg_left, con
 
   pointCloud_pub_.publish(pc);
 
-  imshow("DISP", dmap);
-  imshow("LEFT", tmpL);
-  imshow("RECT-LEFT", img_left);
-  imshow("RECT-LEFT-COLOR", img_left_color);
-  imshow("RIGHT", tmpR);
-  imshow("RECT-RIGHT", img_right);
-  cv::waitKey(30);
+    imshow("DISP", dmap);
+//  imshow("LEFT", tmpL);
+//  imshow("RECT-LEFT", img_left);
+//  imshow("RECT-LEFT-COLOR", img_left_color);
+//  imshow("RIGHT", tmpR);
+//  imshow("RECT-RIGHT", img_right);
+    cv::waitKey(30);
 
-//  if (!mapInitialized_) {
-//    //grid_map::GridMapRosConverter::initializeFromImage(current_msg, resolution_, map_);
-//
-//
-////    //grid_map::GridMapCvConverter::initializeFromImage(dmap,resolution_ ,map_);
-//    const double lengthX = resolution_ * dmap.rows;
-//    const double lengthY = resolution_ * dmap.cols;
-//    grid_map::Length length(lengthX, lengthY);
-//    map_.setGeometry(length, resolution_);
-//
-//    // Grid map settings.
-//    map_.setFrameId(mapFrameId_);
-//    //map_.setGeometry(grid_map::Length(720, 2560), resolution_);
-//    ROS_INFO("Initialized map with size %f x %f m (%i x %i cells).", map_.getLength().x(),
-//         map_.getLength().y(), map_.getSize()(0), map_.getSize()(1));
-//
-//    mapInitialized_ = true;
-//  }
-//
-//
-//  grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 3>(dmap, "elevation", map_, minHeight_, maxHeight_);
-//  grid_map::GridMapCvConverter::addColorLayerFromImage<unsigned char, 3>(dmap, "color", map_);
-//
-//  // Publish grid map.
-//  grid_map_msgs::GridMap message;
-//  grid_map::GridMapRosConverter::toMessage(map_, message);
-//  gridMapPublisher_.publish(message);
+  if (!mapInitialized_) {
+    //grid_map::GridMapRosConverter::initializeFromImage(current_msg, resolution_, map_);
+
+    //grid_map::GridMapCvConverter::initializeFromImage(dmap,resolution_ ,map_);
+    const double lengthX = resolution_ * dmap.rows;
+    const double lengthY = resolution_ * dmap.cols;
+    grid_map::Length length(lengthX, lengthY);
+    map_.setGeometry(length, resolution_);
+
+    // Grid map settings.
+    map_.setFrameId(mapFrameId_);
+    //map_.setGeometry(grid_map::Length(720, 2560), resolution_);
+    ROS_INFO("Initialized map with size %f x %f m (%i x %i cells).", map_.getLength().x(),
+         map_.getLength().y(), map_.getSize()(0), map_.getSize()(1));
+
+    mapInitialized_ = true;
+  }
+
+  grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 3>(dmap, "elevation", map_, minHeight_, maxHeight_);
+  grid_map::GridMapCvConverter::addColorLayerFromImage<unsigned char, 3>(dmap, "color", map_);
+
+  // Publish grid map.
+  grid_map_msgs::GridMap message;
+  grid_map::GridMapRosConverter::toMessage(map_, message);
+  gridMapPublisher_.publish(message);
 
 }
 
